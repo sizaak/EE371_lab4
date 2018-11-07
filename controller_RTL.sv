5a15fa5a345a9f90a4170f87849c38a77d3c7bca
kyuri95 <38845321+kyuri95@users.noreply.github.com>
2018-11-07 13:12:58 -0800

    Bit counter controller

--- controller_RTL.sv
+++ controller_RTL.sv
@@ -1,91 +1,78 @@
-module controller_RTL (Start, Clock, Resetn, A, Ram_Data, L_Bound, R_Bound, 
-								Load_A, Update_L, Update_R, Found_True, Init_Bound, Done); 
+module controller_RTL (s, clock, reset_b, load_A, rightshift_A, result_zero, incr_result, done, z, a0); 
+	output logic load_A, rightshift_A, result_zero, incr_result, done; 
+	input logic s, clock, reset_b, z, a0; 
 	
-	input logic Start, Clock, Resetn;
-	input logic [7:0] A, Ram_Data;
-	input logic [4:0] L_Bound, R_Bound;
-	output logic Load_A, Found_True, Init_Bound, Done, Update_L, Update_R;
+	logic [1: 0] present_state, next_state; 
+	parameter s1 = 2'b00, s2 = 2'b01, s3 = 2'b11; // state codes 
 	
-	enum {S1, S2, S3, S4} present_state, next_state;
 
 	// State transitions (edge sensitive) 
-	always_ff @(posedge Clock) begin 
-		if (Resetn) present_state <= S1; 
+	always_ff @(posedge clock) begin 
+		if (reset_b) present_state <= s1; 
 		else present_state <= next_state; 
 	end
 
 	// code next-state logic directly from ASMD chart 
 	always_comb begin 
 		case (present_state) 
-			S1: begin
-				if(Start) next_state = S2; 
-				else next_state = S1; 
+			s1: begin
+				if (s) next_state = s2; 
+				else next_state = s1; 
 			end
-			S2: begin
-				 if(A == Ram_Data || L_Bound >= R_Bound) next_state = S4;
-				 else next_state = S3;
+			s2: begin
+				if(z) next_state = s3;
+				else next_state = s2; 
 			end
-			S3: begin
-				next_state = S2;
+			s3: begin
+				if (s) next_state = s3; 
+				else next_state = s1; 
 			end
-			S4: begin
-				if (Start) next_state = S4; 
-				else next_state = S1; 
-			end
-			default: next_state = S1; 
+			default: next_state = s1; 
 		endcase 
 	end 
 	
-	assign Load_A = (present_state == S1) && ~Start;
-	assign Found_True = (present_state == S2) && (A == Ram_Data);
-	assign Update_L = (present_state == S2) && (L_Bound != R_Bound) && (A > Ram_Data);
-	assign Update_R = (present_state == S2) && (L_Bound != R_Bound) && (A < Ram_Data);
-	assign Init_Bound = (present_state == S1);
-	assign Done = (present_state == S4);
+	assign load_A = (present_state == s1) && ~s;
+	assign result_zero = (present_state == s1);
+	assign rightshift_A = (present_state == s2) && ~z;
+	assign incr_result = a0 && ~z && (present_state == s2);
+	assign done = (present_state == s3);
 	
 endmodule 
-
+	
+	
+	/*
 	
 module tb_controller();
-	logic Start, Clock, Resetn;
-	logic [7:0] A, Ram_Data;
-	logic [4:0] L_Bound, R_Bound;
-	logic Load_A, Found_True, Init_Bound, Done, Update_L, Update_R;
+	logic LA, EA, EB, LB, done; 
+	logic start, clock, reset_b; 
+	//logic [7:0] A;
 	
 	parameter CLOCK_PERIOD = 100;
 	
-	controller_RTL dut (.Start, .Clock, .Resetn, .A, .Ram_Data, .L_Bound, .R_Bound, 
-								.Load_A, .Update_L, .Update_R, .Found_True, .Init_Bound, .Done);  
+	controller_RTL dut (.start, .clock, .reset_b, .LA, .EA, .EB, .LB, .done); 
+	
 
 	initial begin
-		Clock <= 0;
-		forever#(CLOCK_PERIOD/2) Clock = ~Clock;
+		clock <= 0;
+		forever#(CLOCK_PERIOD/2) clock = ~clock;
 	end
 	
 	initial begin
-		Resetn <= 1; Start <= 0;@(posedge Clock);
-		Resetn <= 0; L_Bound <= 5'd0; R_Bound <= 5'd31; A <= 8'd0; @(posedge Clock);
-		Start <= 1; Ram_Data <= 8'd32; @(posedge Clock);
-		R_Bound <= 5'd14; Ram_Data <= 8'd16;@(posedge Clock);
-		R_Bound <= 5'd6; Ram_Data <= 8'd8; @(posedge Clock);
-		R_Bound <= 5'd2; Ram_Data <= 8'd4; @(posedge Clock);
-		R_Bound <= 5'd0; Ram_Data <= 8'd2; @(posedge Clock);@(posedge Clock);
-		Start <= 0; @(posedge Clock);
-		L_Bound <= 5'd0; R_Bound <= 5'd31; A <= 8'd50; @(posedge Clock);
-		Start <= 1; Ram_Data <= 8'd20; @(posedge Clock);
-		L_Bound <= 5'd16; Ram_Data <= 8'd24;@(posedge Clock);
-		L_Bound <= 5'd24; Ram_Data <= 8'd28; @(posedge Clock);
-		L_Bound <= 5'd28; Ram_Data <= 8'd32; @(posedge Clock);
-		L_Bound <= 5'd30; Ram_Data <= 8'd36; @(posedge Clock);
-		L_Bound <= 5'd31; Ram_Data <= 8'd40; @(posedge Clock);@(posedge Clock);
-		Start <= 0; @(posedge Clock);
-		L_Bound <= 5'd0; R_Bound <= 5'd31; A <= 8'd42; @(posedge Clock);
-		Start <= 1; Ram_Data <= 8'd36; @(posedge Clock);
-		L_Bound <= 5'd16; Ram_Data <= 8'd40;@(posedge Clock);
-		L_Bound <= 5'd24; Ram_Data <= 8'd46; @(posedge Clock);
-		R_Bound <= 5'd26; Ram_Data <= 8'd42; @(posedge Clock);@(posedge Clock);
-		Start <= 0; @(posedge Clock);
+		reset_b <= 1; @(posedge clock);
+		reset_b <= 0; start <= 0; A <= 8'b10101010;@(posedge clock);
+		start <= 1; @(posedge clock);
+		A <= 8'b01010101;@(posedge clock);
+		A <= 8'b00101010;@(posedge clock);
+		A <= 8'b00010101;@(posedge clock);
+		A <= 8'b00001010;@(posedge clock);
+		A <= 8'b00000101;@(posedge clock);
+		A <= 8'b00000010;@(posedge clock);
+		A <= 8'b00000001;@(posedge clock);
+		A <= 8'b00000000;@(posedge clock);
+		start <= 0;@(posedge clock);
+		@(posedge clock);
 		$stop;
 	end
 	
 endmodule
+*/
